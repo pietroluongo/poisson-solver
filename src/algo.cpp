29961f04e11457970dfa5ccf7214ab1ddd492a6d
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-06-19 17:53:20 -0300

    WIP - Mais trabalho no algoritmo

--- src/algo.cpp
+++ src/algo.cpp
@@ -25,6 +25,16 @@ double getOmegaIdeal(int nx, int ny) {
     return ((8 - (sqrt(64-16*t2)))/t2);
 }
 
+double efedexiseipsilom(double x, double y) {
+    double q1 = x * (10-x);
+    double q2 = y * (5-y);
+    return (1.0/5.0) * (q1 + q2);
+}
+
+double vedexisipsilon(double x, double y) {
+    return (x*(10-x) * y * (5-y))/10;
+}
+
 int main() {
     int nx = getNx();
     int ny = getNy();
@@ -35,5 +45,21 @@ int main() {
     c = d = (-1)/(hy*hy);
     e = 2 * ((1 / (hx*hx)) + (1/(hy*hy)));
     printf("a = %lf b = %lf c = %lf d = %lf e = %lf\n\n", a, b, c, d, e);
+    printf("f = \n\n");
+    for(int i = 0; i <= 10; i++) {
+        printf("y = %.2lf -> ", i*hy);
+        for(int j = 0; j <= 5; j++) {
+            printf("%.4lf ", efedexiseipsilom(i, j));
+        }
+        printf("\n");
+    }
+    printf("\nV = \n\n");
+    for(int i = 0; i <= 10; i++) {
+        printf("y = %.2lf -> ", (i*hy));
+        for(int j = 0; j <= 5; j++) {
+            printf("%.4lf ", vedexisipsilon(i, j));
+        }
+        printf("\n");
+    }
     return 0;
 }
