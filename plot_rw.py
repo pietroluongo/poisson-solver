fbdb122f5912653c09af41f3bb94e85386ddeacd
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-06-24 03:01:33 -0300

    Implementação do plot de contornos

--- plot_rw.py
+++ plot_rw.py
@@ -7,31 +7,54 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import argparse
 
-############### DEFINES ########################
+############################ DEFINES ###########################
 
-mode = 'Calculado'
-# mode = 'Exato'
+# mode = 'Calculado'
+mode = 'Exato'
+
+########## Validacao ##########
 
 # Output step = 0.125
 # inFname = 'output_SOR_validacao_0.1250_0.1250.txt'
 
 # Output step = 0.25
-inFname = 'output_SOR_validacao_0.2500_0.2500.txt'
+# inFname = 'output_SOR_validacao_0.2500_0.2500.txt'
 
 # Output step = 0.5
 # inFname = 'output_SOR_validacao_0.5000_0.5000.txt'
 
-# Dom
+# Ground step = 0.5
+# inFname = 'ground_SOR_validacao_0.5000_0.5000.txt'
+
+# Ground step = 0.25
+# inFname = 'ground_SOR_validacao_0.2500_0.2500.txt'
+
+# Ground step = 0.125
+inFname = 'output_SOR_validacao_0.1250_0.1250.txt'
+
+########### Capacitores ##########
+
+# Output Step = 0.5
+# inFname = 'output_SOR_capacitores_0.5000_0.5000.txt'
+
+# Output Step = 0.25
+# inFname = 'output_SOR_capacitores_0.2500_0.2500.txt'
+
+# Output Step = 0.125
+# inFname = 'output_SOR_capacitores_0.1250_0.1250.txt'
+
+
+# Dom - MUST MATCH PROBLEM!!!
 x0 = 0
 x1 = 10
 y0 = 0
 y1 = 5
 
-# Steps
-hx = 0.25
-hy = 0.25
+# Steps - MUST MATCH FILENAME!!!
+hx = 0.125
+hy = 0.125
 
-###############################################
+#################################################################
 
 def main():
     inputFile = open(inFname, 'r')
@@ -43,9 +66,23 @@ def main():
     ny = 1 + ((y1-y0)/hy)
     arr = np.array(formData)
     arr = arr.reshape((int(ny), int(nx)))
+    plot_contour(arr, nx, ny)
     plot_2d(arr, nx, ny)
     plot_3d(arr, nx, ny)
     return
+
+def plot_contour(data, nx, ny):
+    X = np.arange(x0, x1+hx, hx)
+    Y = np.arange(y0, y1+hy, hy)
+    X, Y = np.meshgrid(X, Y)
+    fig, ax = plt.subplots()
+    CS = ax.contour(X, Y, data)
+    ax.clabel(CS, inline=1, fontsize=10)
+    ax.set_title(mode)
+    # Doesnt make much sense here...
+    # fig.colorbar(CS)
+    plt.show()
+    return    
 
 def plot_2d(data, nx, ny):
     X = np.arange(x0, x1, hx)
@@ -58,7 +95,7 @@ def plot_2d(data, nx, ny):
 
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
             rotation_mode="anchor")
-
+    fig.colorbar(im)
     ax.set_title(mode)
     fig.tight_layout()
     plt.show()
@@ -74,6 +111,7 @@ def plot_3d(data, nx, ny):
     surf = ax.plot_surface(X, Y, data, cmap=cm.jet, 
                            linewidth=0, antialiased=False)
     fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_title(mode)
     plt.show()
     return
 
